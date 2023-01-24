be401e70518f13128e77c481adbb1f40665cfa73
Räghib Hasan <raghibmidhat@gmail.com>
2023-01-24 19:08:09 +0200

    feat(app): add testing library

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+// Add custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+
+    '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+module.exports = createJestConfig(customJestConfig)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,18 @@
+import Head from 'next/head'
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Head>
+        <title>{`Raghib's blog`}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to Raghib&apos;s Blog</h1>
+      </main>
+
+      <footer></footer>
+    </div>
+  )
 }
