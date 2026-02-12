e9989d48e4fbbe20b133885dd7fce874c75772b6
Räghib Hasan <ragmha@users.noreply.github.com>
2026-02-12 22:57:31 +0200

    fix: mobile responsiveness, add unit tests, fix LinkedIn URL

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from 'next'
-import { JetBrains_Mono } from 'next/font/google'
+import { Inter, IBM_Plex_Mono } from 'next/font/google'
 import '@/styles/globals.css'
 import { cn } from '@/lib/utils'
 import { ThemeProvider } from '@/components/theme-provider'
 
-const fontMono = JetBrains_Mono({ subsets: ['latin'], variable: '--font-mono' })
+const fontSans = Inter({ subsets: ['latin'], variable: '--font-sans' })
+const fontMono = IBM_Plex_Mono({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+  variable: '--font-mono',
+})
 
 export const metadata: Metadata = {
   title: 'Raghib Hasan',
@@ -20,7 +25,8 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          'min-h-screen bg-background font-mono antialiased',
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable,
           fontMono.variable
         )}
       >
